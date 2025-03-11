1c78ab9a9ffac3bf60c2969257c2ee221cb20b2c
CThaw90 <cthaw@mediaocean.com>
2025-03-11 19:56:40 -0400

    Created TextNode class and main.py entrypoint

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+    print(TextNode('Navigate to the Theezy Trader app', TextType.LINK, 'https://theezy-trader-da96c.ondigitalocean.app/'))
+
+
+if __name__ == '__main__':
+    main()
