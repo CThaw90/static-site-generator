ba16e78a8c9fc17c6ca070dd8d28330fd01b66e0
CThaw90 <cthaw@mediaocean.com>
2025-03-11 22:03:48 -0400

    Created LeafNode class with unit testing

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -10,3 +10,18 @@ class HTMLNode:
 
     def __repr__(self):
         return f'HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})'
+
+
+class LeafNode(HTMLNode):
+
+    def __init__(self, value, tag=None, props=None):
+        super().__init__(tag, value, None, props)
+
+        if value is None:
+            raise ValueError('value cannot be None')
+
+        self.value = value
+        self.tag = tag
+
+    def to_html(self):
+        return f'<{self.tag}>{self.value}</{self.tag}>' if self.tag else self.value
